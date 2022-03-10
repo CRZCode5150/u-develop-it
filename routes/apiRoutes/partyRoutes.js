672c3f585442c32b702e1a27c65d321fa263e009
Chris <chrisramirez15@yahoo.com>
2022-03-10 14:16:25 -0600

    added routes for post, put, delete

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+//ROute for all parties
+router.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  router.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+  // Get single party
+router.get('/party/:id', (req, res) => {});
+
+// Delete a party
+router.delete('/party/:id', (req, res) => {});
+
+module.exports = router;
